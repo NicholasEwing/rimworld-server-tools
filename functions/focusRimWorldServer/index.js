45effddd5b7a05ff12a861d2da3ed57fbfc71154
Nicholas Ewing <nicholassewing@gmail.com>
2022-04-09 10:32:04 -0400

    Adds conditions and errors for focusRimWorldServer()

--- functions/focusRimWorldServer/index.js
+++ functions/focusRimWorldServer/index.js
@@ -1,17 +1,25 @@
 const processWindows = require("node-process-windows");
 
 module.exports = async function focusRimWorldServer() {
-  //   const activeProcesses = processWindows.getProcesses(function (
-  //     err,
-  //     processes
-  //   ) {
-  //     processes.forEach(function (p) {
-  //       console.log("PID: " + p.pid.toString());
-  //       console.log("MainWindowTitle: " + p.mainWindowTitle);
-  //       console.log("ProcessName: " + p.processName);
-  //     });
-  //   });
-  processWindows.focusWindow("Open World");
-  console.log("Focus RimWorld Server ran!");
+  let activeWindow;
+  const activeProcesses = processWindows.getProcesses(function (
+    err,
+    processes
+  ) {
+    let openWorldProcesses = processes.filter(
+      (p) => p.processName.indexOf("Open World") >= 0
+    );
+
+    if (openWorldProcesses.length > 0) {
+      processWindows.focusWindow(openWorldProcesses[0]);
+      console.log("Focused RimWorld Server!");
+      return;
+    } else {
+      throw new Error(
+        "RimWorld server is not running! Instead of rerunning this script, please go launch your Open World server and use the 'reload' command."
+      );
+    }
+  });
+
   return;
 };
